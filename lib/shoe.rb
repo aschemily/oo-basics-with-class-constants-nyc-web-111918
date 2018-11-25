3621e82655e2bcc0a0114fcd8559da2ab26f5631
Emily Asch <e.asch@me.com>
2018-11-25 21:40:33 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,14 @@
 class Shoe 
   
-attr_reader :brands 
+attr_reader :brand
 
-
+BRANDS = []
+  def initialize(brand)
+    @brand = brand
+    
+    BRANDS << brand 
+    
+  end
   
 end #end of shoe class 
 
