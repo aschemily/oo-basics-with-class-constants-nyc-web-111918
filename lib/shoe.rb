98e134435e2ecd99bc7dfd079d1dd5ba4762c57a
Emily Asch <e.asch@me.com>
2018-11-25 21:04:03 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,8 +4,12 @@ class Shoe
 
 @@all = []
 
-  def initialize(brand)
+  def initialize(brand, color, size, material, condition)
     @brand = brand
+    @color = color 
+    @size = size 
+    @material = material 
+    @condition = condition 
     
     @@all << self 
   end
